59e5b5cbd2316f9b6b90f15b0fd6a781fd04f9ef
JG Rivera <rivera.jga10@gmail.com>
2020-10-10 14:08:59 +0800

    URL setup

---
+++ core/urls.py
@@ -0,0 +1,10 @@
+"""
+    Class for defining URL routes
+"""
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
--- core/views.py
+++ core/views.py
@@ -4,3 +4,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    """
+        Render the index page
+    """
+    return render(request, 'index.html', {})
