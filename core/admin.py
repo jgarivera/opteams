2c5c9ec80cc442de6258440b54ed71a5a191ee12
JG Rivera <rivera.jga10@gmail.com>
2020-10-20 23:44:57 +0800

    Simple admin customization

--- core/admin.py
+++ core/admin.py
@@ -4,6 +4,17 @@
 from django.contrib import admin
 from .models import Channel, Assignment
 
+
+class ChannelAdmin(admin.ModelAdmin):
+    # Define field sections
+    fieldsets = [
+        ("Teams Data", {"fields": ["uuid", "name", "description", "url"]}),
+        ("App Metadata", {"fields": ["subject_name", "subject_code", "connector_key"]}),
+    ]
+
+    # Define list display when viewing collectively
+    list_display = ("name", "subject_code")
+
 # Register your models here.
-admin.site.register(Channel)
+admin.site.register(Channel, ChannelAdmin)
 admin.site.register(Assignment)
