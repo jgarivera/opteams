9e3cceedf3e0b969232ba439ed080237f0f6a189
JG Rivera <rivera.jga10@gmail.com>
2020-10-20 23:26:03 +0800

    Admin setup

--- core/admin.py
+++ core/admin.py
@@ -2,5 +2,8 @@
     Class for configuring the admin dashboard
 """
 from django.contrib import admin
+from .models import Channel, Assignment
 
 # Register your models here.
+admin.site.register(Channel)
+admin.site.register(Assignment)
