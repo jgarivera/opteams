898a165210d854c5dbc18bbdb8e4313dbd2b0a9c
JG Rivera <rivera.jga10@gmail.com>
2020-10-21 17:12:43 +0800

    Assignment admin

--- core/admin.py
+++ core/admin.py
@@ -5,16 +5,25 @@ from django.contrib import admin
 from .models import Channel, Assignment
 
 
+class AssignmentAdmin(admin.ModelAdmin):
+    # Define field sections
+    fieldsets = [
+        ("Teams Data", {"fields": ["title", "subtitle", "url"]}),
+        ("Date Information", {"fields": ["date_posted", "date_due"]}),
+    ]
+    list_display = ("title", "channel")
+
 class ChannelAdmin(admin.ModelAdmin):
     # Define field sections
     fieldsets = [
         ("Teams Data", {"fields": ["uuid", "name", "description", "url"]}),
-        ("App Metadata", {"fields": ["subject_name", "subject_code", "connector_key"]}),
+        ("App Data", {"fields": ["subject_name", "subject_code", "connector_key"]}),
     ]
 
     # Define list display when viewing collectively
     list_display = ("name", "subject_code")
 
+
 # Register your models here.
 admin.site.register(Channel, ChannelAdmin)
-admin.site.register(Assignment)
+admin.site.register(Assignment, AssignmentAdmin)
