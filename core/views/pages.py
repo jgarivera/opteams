4ac3ebac86f35b8e355ce59ca7ac88e3817d0614
JG Rivera <rivera.jga10@gmail.com>
2020-10-24 11:05:05 +0800

    Subscription integration

--- core/views/pages.py
+++ core/views/pages.py
@@ -14,19 +14,28 @@ import stream
 @login_required
 def index(request):
     """
-        Render the index page
+    Render the index page
     """
-    return render(request, "index.html", {})
+
+    # Retrieve all assignments from user's channel subscriptions
+    profile = request.user.userprofile
+    subscribed_channels = profile.subscriptions.all()
+    assignments = Assignment.objects.in_date_due_order(channel__in=subscribed_channels)
+
+    context = {"assignments": assignments}
+    return render(request, "index.html", context)
 
 
 @login_required
 def calendar(request):
     """
-        Render the calendar page
+    Render the calendar page
     """
 
-    # Retrieve all assignments
-    assignments = Assignment.objects.all()
+    # Retrieve all assignments from user's channel subscriptions
+    profile = request.user.userprofile
+    subscribed_channels = profile.subscriptions.all()
+    assignments = Assignment.objects.in_date_due_order(channel__in=subscribed_channels)
 
     context = {"assignments": assignments}
     return render(request, "calendar.html", context)
@@ -35,14 +44,14 @@ def calendar(request):
 @login_required
 def notifications(request):
     """
-        Render notifications page
+    Render notifications page
     """
     # Create stream client
     client = stream.connect(settings.STREAM_IO_KEY, settings.STREAM_IO_SECRET)
 
     # Retrieve notifications
     notifications = client.feed("notification", request.user.username)
-    activities = notifications.get(limit=10)["results"]
-
+    activities = notifications.get(limit=10)["results"][0]["activities"]
+    
     context = {"activities": activities}
-    return render(request, "index.html", context)
+    return render(request, "notifications.html", context)
