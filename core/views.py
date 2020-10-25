f7c728fc1cb52e08e55b2cd17ba9b41150039e89
DesireeCoder <60638686+DemeterCaubang@users.noreply.github.com>
2020-10-26 01:24:21 +0800

    first frontend commit render pages

--- core/views.py
+++ core/views.py
@@ -8,4 +8,25 @@ def index(request):
     """
         Render the index page
     """
-    return render(request, 'index.html', {})
+    return render(request, 'index.html')
+
+def main_list(request):
+    return render(request, 'main_list.html')
+
+def main_grid(request):
+    return render(request, 'main_grid.html')
+
+def notifications(request):
+    return render(request, 'notifications.html')
+
+def calendar(request):
+    return render(request, 'calendar.html')
+
+def task_list(request):
+    return render(request, 'task_list.html')
+
+def settings(request):
+    return render(request, 'settings.html')
+
+def add(request):
+    return render(request, 'add.html')
